e792cc7ac8978b788583b5ff52d7c93c01ebd672
lynxknight <decimal.counter@gmail.com>
2016-11-11 22:39:15 +0300

    Basic StatusBar app with quit MenuItem

--- EZTomatoTimer/AppDelegate.swift
+++ EZTomatoTimer/AppDelegate.swift
@@ -12,16 +12,24 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
 
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        print("Application did finish launching")
+        statusItem.title = "TomatoTimer"
+        statusItem.menu = self.statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @IBAction func quitMenuItemClicked(_ sender: NSMenuItem) {
+        print("quitMenuItemClicked, exiting...")
+        NSApplication.shared().terminate(self)
+    }
 
 }
 
